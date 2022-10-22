dc1961c2791c0f978bcbb635491eea89f6fc4501
Crystal Redmon <cato2848@gmail.com>
2022-10-22 12:12:56 -0500

    select colors

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -46,7 +46,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Flora Finds</h1>
+        <h1 className="App-title">Floral Finds</h1>
       </header>
       <main className="gallery_container">
         <GalleryList
